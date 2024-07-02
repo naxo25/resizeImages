e98624c3f0c769fa03439314f0d5b5ec69cb0e1c
naxo25 <y.labra.25@gmail.com>
2024-07-02 15:44:13 -0400

    add button copy data:image

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 	<main class='h-screen'>
 		<h1 class='pt-11 absolute flex items-center justify-center gap-x-2 w-full text-center'>
 			<img src="/ico.png">
-			CreateIcons
+			resizeImages
 		</h1>
 
 		<!-- Paste Zone -->
@@ -20,8 +20,8 @@
 				<p class="mt-2 text-xs tracking-wide text-gray-500">Upload or paste your image</p>
 
 				<div class="text-center">
-					<p class="mt-8 font-medium tracking-wide text-gray-700">Size</p>
-					<input v-model='size' class="mt-2 text-center font-medium text-gray-700" placeholder="Size Icon"/>
+					<p class="mt-8 font-medium tracking-wide text-gray-700">New Size of image/icon</p>
+					<input v-model='size' class="mt-2 border text-center font-medium text-gray-700" placeholder="default 32"/>
 				</div>
 			</div>
 
@@ -30,29 +30,37 @@
 
 		<div v-else class='grid place-content-center items-center mx-auto h-screen grid-cols-2 w-10/12'>
 			<div>
-				<img class="mx-auto rounded-xl" :src="image1" />
+				<img id='img1' class="mx-auto rounded" :src="image1" />
 				<p class='text-center mt-8 text-gray-800'>Imagen original</p>
 			</div>
 
 			<div class="text-center cursor-pointer">
-				<img class="mx-auto rounded-xl" onclick='document.getElementById("a").click()' :src="image2" />
-				<a :href='image2' download id='a' class='mt-2 block text-sky-500'>Descargar</a>
+				<img class="mx-auto rounded" onclick='document.getElementById("a").click()' :src="image2" />
+				<a :href='image2' download id='a' class='mt-2 block text-sky-500'>Descargar imagen</a>
+				<p @click='copyData' class='mt-2 block text-sky-500'>Copiar data:image</p>
 				<p @click='image1 = false' class='mt-2'>Empezar de nuevo</p>
 			</div>
 		</div>
 	</main>
+	<Toaster position="bottom-right" />
 </template>
 
 <script setup>
 	import { ref } from 'vue'
+	import { Toaster, toast } from 'vue-sonner'
 	import ImageTools from '@/redim.js'
 
 	const image2 = ref()
 	const image1 = ref()
-	const size = ref(32)
+	const size = ref()
 
 	document.onpaste = (pasteEvent) => {
 		const event = pasteEvent.clipboardData.items[0]
+
+		if (event.type.indexOf('text') === 0) {
+			// image1.value = pasteEvent.clipboardData.getData('text')
+			return
+		}
 
 		if (event.type.indexOf('image') === 0) {
 			const blob = event.getAsFile()
@@ -61,7 +69,7 @@
 			reader.onload = (event) => image1.value = event.target.result
 			reader.readAsDataURL(blob)
 
-	    resizeTools(blob, size.value)
+	    resizeTools(blob, size.value || 32)
 		}
 	}
 
@@ -71,7 +79,7 @@
 	  reader.onload = (event) => image1.value = event.target.result
 	  reader.readAsDataURL(event.target.files[0])
 
-    resizeTools(event.target.files[0], size.value)
+    resizeTools(event.target.files[0], size.value || 32)
 	}
 
 	const resizeTools = (event, calidad) => {
@@ -81,5 +89,10 @@
       reader.onload = (event) => image2.value = event.target.result
       reader.readAsDataURL(blob)
 		})
+	}
+
+	const copyData = () => {
+		navigator.clipboard.writeText(image2.value);
+		toast("Contenido copiado al portapapeles.");
 	}
 </script>
